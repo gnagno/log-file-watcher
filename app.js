217048d5e33c6e9c43db65614cd00670e725929c
Florent Jaby <florent.jaby@gmail.com>
2011-02-25 01:20:47 +0100

    added logger to see what's going on

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.logger({format: ":method :url :date from :remote-addr :user-agent -> :status\n"}));
   app.use(express.bodyDecoder());
   app.use(express.methodOverride());
   app.use(app.router);
